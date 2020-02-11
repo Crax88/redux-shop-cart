82f66eac08b7de52415e373ed1b71d7e57d99674
Nikita <ntrafilkin88@gmail.com>
2020-02-11 16:36:22 +0300

    adds connection to redux and service

--- src/components/BooksList/BooksList.js
+++ src/components/BooksList/BooksList.js
@@ -1,14 +1,22 @@
 import React, { Component } from "react";
 import BooksListItem from "../BooksListItem/BookListItem";
-
-export default class BooksList extends Component {
+import { connect } from "react-redux";
+import WithBookStoreService from "../hocs/WithBookStoreService";
+import { booksLoaded } from "../../store/actions/index";
+import { compose } from "../../utils/index";
+class BooksList extends Component {
+  componentDidMount() {
+    const { bookStoreService } = this.props;
+    const data = bookStoreService.getBooks();
+    this.props.booksLoaded(data);
+  }
   render() {
     const { books } = this.props;
     return (
       <ul>
         {books.map(book => {
           return (
-            <li>
+            <li key={book.id}>
               <BooksListItem book={book} />
             </li>
           );
@@ -17,3 +25,27 @@ export default class BooksList extends Component {
     );
   }
 }
+
+const mapStateToProps = ({ books }) => {
+  return { books };
+};
+
+const mapDispatchToProps = {
+  booksLoaded
+};
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     booksLoaded: booksList => dispatch(booksLoaded(booksList))
+//   };
+// };
+// const mapDispatchToProps = dispatch => {
+//   return bindActionCreators({ booksLoaded }, dispatch);
+// };
+export default compose(
+  WithBookStoreService,
+  connect(mapStateToProps, mapDispatchToProps)
+)(BooksList);
+
+// WithBookStoreService(
+//   connect(mapStateToProps, mapDispatchToProps)(BooksList)
+// );
--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,15 +1,7 @@
 import React from "react";
 import BooksList from "../BooksList/BooksList";
 const HomePage = () => {
-  const books = [
-    {
-      id: 1,
-      title: "Production-Ready Microservices",
-      author: "Susan J. Fowler"
-    },
-    { id: 2, title: "Release It!", author: "Michael T. Nygard" }
-  ];
-  return <BooksList books={books} />;
+  return <BooksList />;
 };
 
 export default HomePage;
